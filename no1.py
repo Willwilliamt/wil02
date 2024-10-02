e977fef9a30d592ecd99ac8ee7691e0c5f4cf12d
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-10-03 01:58:54 +0700

    Add files via upload

---
+++ no1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+import matplotlib.pyplot as plt
+order_items = pd.read_csv('order_items_dataset.csv')
+order_payments = pd.read_csv('order_payments_dataset.csv')
+order_reviews = pd.read_csv('order_reviews_dataset.csv')
+orders = pd.read_csv('orders_dataset.csv')
+products = pd.read_csv('products_dataset.csv')
+product_translation = pd.read_csv('product_category_name_translation.csv')
+sellers = pd.read_csv('sellers_dataset.csv')
+customers = pd.read_csv('customers_dataset.csv')
+geolocation = pd.read_csv('geolocation_dataset.csv')
+orders_items_merged = pd.merge(orders, order_items, on='order_id')
+full_data = pd.merge(orders_items_merged, customers, on='customer_id')
+full_data_with_products = pd.merge(full_data, products, on='product_id')
+top_products = full_data_with_products.groupby('product_id')['price'].sum().reset_index()
+top_products = pd.merge(top_products, products[['product_id', 'product_category_name']], on='product_id')
+top_products = top_products.sort_values(by='price', ascending=False).head(10)
+print(top_products)
+st.title('Dashboard Analisis E-Commerce')
+st.header('10 Produk dengan Penjualan Tertinggi')
+st.dataframe(top_products)
+plt.figure(figsize=(10,6))
+sns.barplot(x='product_category_name', y='price', data=top_products)
+plt.xticks(rotation=90)
+plt.title('10 Produk dengan Penjualan Tertinggi')
+st.pyplot(plt)
